fba43d385f73b40ea486bc85bc401beff4211041
Damien Beaufils <damien.beaufils@gmail.com>
2017-09-07 17:26:28 +0200

    Initial import

---
+++ src/main/java/com/example/demo/CleanArchitectureDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureDemoApplication.class, args);
+	}
+}
